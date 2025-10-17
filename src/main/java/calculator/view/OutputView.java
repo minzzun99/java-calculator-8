76f83287c5749330d52d4bfdf8f4d2fe7a4852ba
minzzun99 <skg06123@naver.com>
2025-10-17 15:48:13 +0900

    feat: 계산 결과 출력 기능 구현
    - 덧셈한 계산 결과 출력

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+public class OutputView {
+    private static final String RESULT_MESSAGE = "결과 : %d";
+
+    public static void printResult(int result) {
+        System.out.printf(RESULT_MESSAGE, result);
+    }
+}
