ce23c1627b00f72d82e38a2ecb5d4fcfb266ead6
minzzun99 <skg06123@naver.com>
2025-10-17 20:31:25 +0900

    feat: 구분자 관련 로직을 담당하는 Delimiter 클래스 구현
    - 커스텀 구분자 존재 여부 확인 기능 구현
    - 구분자 기준으로 숫자 분리 기능 구현
    - 커스텀 구분자 입력 포멧 검증
    - 커스텀 구분자 갯수 검증
    - 커스텀 구분자 숫자 입력 여부 검증

---
+++ src/main/java/calculator/model/Delimiter.java
@@ -0,0 +1,76 @@
+package calculator.model;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Delimiter {
+    private final Set<Character> delimiters;
+
+    public Delimiter() {
+        delimiters = new HashSet<>(Arrays.asList(',', ':'));
+    }
+
+    public String[] split(String input) {
+        if (checkCustomDelimiter(input)) {
+            input = validateCustomDelimiter(input);
+        }
+
+        return input.split(createRegex());
+    }
+
+    private String createRegex() {
+        StringBuilder regex = new StringBuilder("[");
+        for (char delimiter : delimiters) {
+            regex.append(delimiter);
+        }
+        regex.append("]");
+        return regex.toString();
+    }
+
+    private void addCustomDelimiter(char delimiter) {
+        delimiters.add(delimiter);
+    }
+
+    // 커스텀 구분자 존재 여부 확인
+    private boolean checkCustomDelimiter(String input) {
+        return input.startsWith("//");
+    }
+
+    // 커스텀 구분자 검증
+    private String validateCustomDelimiter(String input) {
+        int index = validateInputFormat(input);
+        char customDelimiter = validateLength(input, index);
+        validateNumericValue(customDelimiter);
+
+        addCustomDelimiter(customDelimiter);
+        return input.substring(index + 2);
+    }
+
+    // 커스텀 구분자 입력 포멧 검증
+    private int validateInputFormat(String input) {
+        int index = input.indexOf("\\n");
+        if (index == -1) {
+            throw new IllegalArgumentException("커스텀 구분자는 '//'와 '\\n' 사이의 문자로 입력 가능합니다.");
+        }
+
+        return index;
+    }
+
+    // 커스텀 구분자 갯수 검증
+    private char validateLength(String input, int index) {
+        String customLine = input.substring(2, index);
+        if (customLine.length() != 1) {
+            throw new IllegalArgumentException("커스텀 구분자는 반드시 하나의 문자로 구성되어야 합니다.");
+        }
+
+        return customLine.charAt(0);
+    }
+
+    // 커스텀 구분자 숫자 여부 검증
+    private void validateNumericValue(char customDelimiter) {
+        if (customDelimiter >= '0' && customDelimiter <= '9') {
+            throw new IllegalArgumentException("커스텀 구분자는 숫자를 사용할 수 없습니다.");
+        }
+    }
+}
